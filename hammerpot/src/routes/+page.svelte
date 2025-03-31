e5ffc7d013988d7c3a8e3cb690025ba3e669bbcb
HammerPot <171651869+HammerPot@users.noreply.github.com>
2025-03-31 02:02:50 -0400

    What is a commit?

--- hammerpot/src/routes/+page.svelte
+++ hammerpot/src/routes/+page.svelte
@@ -1,7 +1,31 @@
 <h1 class="text-5xl text-center m-4 pt-1"> HammerPot</h1>
-<p class="text-base mx-32"> Welcome! Here you can find my projects, music, photos, and more. </p>
+<h3 class="text-3xl mx-32"> Welcome!</h3>
+<br>
+<div class="flex flex-col lg:flex-row items-center justify-center lg:justify-start mt-3 mx-4 lg:mx-32 ">
+    <img src="/images/hammerpot.png" alt="HammerPot Logo" class="rounded-full w-1/5 m-4">
+    <div class="self-start flex-1 mx-2">
+        <h3 class="text-3xl text-center mt-5 mb-2">About Me</h3>
+        <br>
+        <p class="text-base mx-5 text-center"> I'm HammerPot! This is my website! Here I will put my various projects that I am ready to share with the world! I like to take photos, listen to music, play video games, and code.  </p>
+    </div>
+    <div class="self-start flex flex-col items-center mt-5 mb-2">
+        <h3 class="text-3xl pb-2">Coding Projects</h3>
+        <br>
+        <div class="grid grid-cols-2 gap-4 pb-6">
+            <div>
+                <h4 class="text-xl pb-2">Singular Card</h4>
+                <p>Uno game made with Python</p>
+            </div>
+            <div>
+                <h4 class="text-xl pb-2">Languages I'm Learning</h4>
+                <p>C, Python, JavaScrip, C#</p>
+            </div>
+        </div>
+    </div> 
+</div>
 
-<div class ="justify-center m-4 border-2 rounded-md mx-32">
+
+<!-- <div class ="justify-center m-4 border-2 rounded-md mx-32">
     <h3 class="text-3xl text-center m-2 pt-1">Coding Languages</h3>
     <div class="grid grid-cols-2 gap-4 pb-6">
         <div class="flex flex-col items-center pt-2">
@@ -13,5 +37,5 @@
             <p>C, Python, JavaScript</p>
         </div>
     </div>
-</div>
+</div> -->
 
