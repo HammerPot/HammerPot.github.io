0b18b73c166d1d869284b093ab4eaa79b97a7c33
HammerPot <171651869+HammerPot@users.noreply.github.com>
2025-03-31 14:15:57 -0400

    Migrated working directory to root
    And fixed homepage to be somewhat normal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,26 @@
 <br>
 <div class="flex flex-col lg:flex-row items-center justify-center lg:justify-start mt-3 mx-4 lg:mx-32 ">
     <img src="/images/hammerpot.png" alt="HammerPot Logo" class="rounded-full w-1/5 m-4">
-    <div class="self-start flex-1 mx-2">
+    <div class="lg:self-start flex-1 mx-2 justify-items-center">
         <h3 class="text-3xl text-center mt-5 mb-2">About Me</h3>
         <br>
         <p class="text-base mx-5 text-center"> I'm HammerPot! This is my website! Here I will put my various projects that I am ready to share with the world! I like to take photos, listen to music, play video games, and code.  </p>
     </div>
-    <div class="self-start flex flex-col items-center mt-5 mb-2">
+    <div class="lg:self-start flex flex-1 flex-col items-center mt-5 mb-2">
         <h3 class="text-3xl pb-2">Coding Projects</h3>
         <br>
-        <div class="grid grid-cols-2 gap-4 pb-6">
+        <div class="grid grid-cols-2 gap-10 pb-6">
             <div>
-                <h4 class="text-xl pb-2">Singular Card</h4>
+                <h4 class="text-xl pb-2"><a href="https://github.com/HammerPot/SingularCard">Singular Card</a></h4>
                 <p>Uno game made with Python</p>
             </div>
             <div>
-                <h4 class="text-xl pb-2">Languages I'm Learning</h4>
-                <p>C, Python, JavaScrip, C#</p>
+                <h4 class="text-xl pb-2"><a href="https://github.com/HammerPot/HammerPot.github.io">HammerPot.dev</a></h4>
+                <p>This website made with Svelte</p>
+            </div>
+            <div class="col-span-2 justify-items-center ">
+                <h4 class="text-xl pb-2 ">More Soon</h4>
+                <p>More projects coming soon!</p>                
             </div>
         </div>
     </div> 
