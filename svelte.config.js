e7adaf013cc45de2de53b669ec7a288ced01dbb1
HammerPot <171651869+HammerPot@users.noreply.github.com>
2025-04-03 18:27:44 -0400

    404 page works!

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: "404.html",
+			fallback: "404",
 			precompress: false,
 			strict: true
 		})
