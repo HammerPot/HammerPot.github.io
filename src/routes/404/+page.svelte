05a8f109208fd172f6a5c72fe7c4b6724a9a2b75
HammerPot <171651869+HammerPot@users.noreply.github.com>
2025-04-02 23:35:07 -0400

    Prepare for github pages

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,6 @@
+<br>
+<h1 class="text-5xl text-center m-4 pt-1">404</h1>
+<h3 class="text-3xl text-center mx-32"> Page Not Found</h3>
+<div class="justify-items-center ">
+	<img src="/images/hammerpot.png" alt="Logo" class="rounded-full w-1/5 m-4">
+</div>
