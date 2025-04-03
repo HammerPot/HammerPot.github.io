1c0ee8e4f337beee6bf71549cdae7e437ba6663d
HammerPot <171651869+HammerPot@users.noreply.github.com>
2025-04-03 18:30:56 -0400

    Revert "404 page works!"
    This reverts commit e7adaf013cc45de2de53b669ec7a288ced01dbb1.

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: "404",
+			fallback: "404.html",
 			precompress: false,
 			strict: true
 		})
